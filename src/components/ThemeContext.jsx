b44c627c580d6bf54da01d0d656d26f2d0187ee4
NishantkSingh0 <nishant18x.org@gmail.com>
2025-03-02 13:43:53 +0530

    Added random chooses ondark mode

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(Math.random() < 0.5);
   console.log("is ",isDark,"From ThemeContext.jsx");
 
   return (
