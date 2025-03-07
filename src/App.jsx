7693c2874ecff29b18ab50242e62284bc3091de6
NishantkSingh0 <nishant18x.org@gmail.com>
2025-03-07 20:38:46 +0530

    __

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import FrontPage from './components/FrontPage.jsx';
 import GetInfo from './components/GetInfo.jsx';
 import Result from './components/Result.jsx';
-// import Examples from './components/Examplepage.jsx';
 import AboutUs from './components/AboutUs.jsx';
 import { Toaster } from "react-hot-toast";
 import { ThemeContext } from './components/ThemeContext.jsx';
 import GoogleVarification from './components/GoogleVarification.jsx';
+// import VarifyMob from './components/VarifyMob.jsx';
 
 const App = () => {
 
@@ -16,16 +16,14 @@ const App = () => {
   useEffect(() => {
     if (isDark) {
       document.documentElement.classList.add("dark");
-      console.log("is dark from App.jsx");
     } else {
       document.documentElement.classList.remove("dark");
-      console.log("is light from App.jsx");
     }
   }, [isDark]);
 
   return (
     // <>
-    //   <ExpandButton/>
+    //   <VarifyMob/>
     // </>
     <div>
       <Toaster />
@@ -33,7 +31,6 @@ const App = () => {
         <Route path="/" element={<FrontPage />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/VarifyMail" element={<GoogleVarification />} />
-        {/* <Route path="/Examples" element={<Examples />} /> */}
         <Route path="/GetInfo" element={<GetInfo />} />
         <Route path="/Result" element={<Result />} /> 
       </Routes>  
--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -4,7 +4,6 @@ export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(Math.random() < 0.5);
-  console.log("is ",isDark,"From ThemeContext.jsx");
 
   return (
     <ThemeContext.Provider value={{ isDark, setIsDark }}>
